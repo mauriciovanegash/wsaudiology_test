acc0a68bf08c188bc5ba3012da2e4cc411812774
Mauricio Vanegas <mauriciovanegash@gmail.com>
2020-09-17 11:42:29 +0200

    Change: Functionality for code-maker class has been implemented

--- src/code_maker.cpp
+++ src/code_maker.cpp
@@ -1,13 +1,80 @@
 #include <stdio.h>
+#include <algorithm>
 #include "code_maker.h"
 
-void code_maker::init_game()
+void code_maker::init_game(state_t value)
 {
     printf("Maker: Initialising game!!!\n");
+    printf("The KEY: [%d, %d, %d, %d]\n", value.code.peg1, value.code.peg2, value.code.peg3, value.code.peg4);
+    _key = value.code;
 }
 
-void code_maker::play_round()
+void code_maker::play_round(state_t &value)
 {
-    printf("Maker: Playing round %d!!!\n", _round);
+    _check_code(value);
+    if (value.assess.right_position != 0)
+        printf("Maker: Got %d right positions!!!\n", value.assess.right_position);
+    if (value.assess.right_colours != 0)
+        printf("Maker: Got %d right colors!!!\n", value.assess.right_colours);
     _round++;
+}
+
+/**
+ * \brief Code check
+ * This function compares the master code with the
+ * code provided in \param value. It makes the evaluation
+ * taking into consideration that the number of pegs is
+ * 4, and two pegs can not have the same color in the
+ * master code.
+ * \param value: Reference to game state - it is of type "state_t"
+ */
+void code_maker::_check_code(state_t &value)
+{
+    color *code = static_cast<color *>(&value.code.peg1);
+    color *master_code = static_cast<color *>(&_key.peg1);
+
+    // Reset counters
+    value.assess.right_position = 0;
+    value.assess.right_colours = 0;
+
+    // Check for right position
+    for (auto i=0; i<4; i++)
+    {
+        if (*(code++) == *(master_code++))
+            value.assess.right_position++;
+    }
+    // Check for right color
+    std::vector<color>colors = _list_colors(value.code);
+    master_code = static_cast<color *>(&_key.peg1);
+    for (auto it=colors.begin(); it!=colors.end(); it++)
+    {
+        for (auto i=0; i<4; i++)
+        {
+            if (master_code[i] == *it)
+            {
+                value.assess.right_colours++;
+                break;
+            }
+        }
+    }
+    // At this point the number of right colors give an account
+    // of the ones in the right position and so, they need to 
+    // be removed
+    value.assess.right_colours -= value.assess.right_position;
+}
+
+/**
+ * \brief Color list
+ * This function creates a vector with unique elements representing
+ * the colors in \param value. It is used to determine how many
+ * colors the "breaker player" has successfully guessed.
+ * \param value: A code value of type "code_t"
+ * \return a std::vector<color>
+ */
+std::vector<color> code_maker::_list_colors(code_t value)
+{
+    std::vector<color> color_list{value.peg1, value.peg2, value.peg3, value.peg4};
+    std::sort(color_list.begin(), color_list.end());
+    color_list.erase(std::unique( color_list.begin(), color_list.end() ), color_list.end());
+    return color_list;
 }
--- src/code_maker.h
+++ src/code_maker.h
@@ -1,6 +1,7 @@
 #ifndef CODE_MAKER_H
 #define CODE_MAKER_H
 
+#include <vector>
 #include "base_player.h"
 
 class code_maker: public base_player
@@ -9,11 +10,37 @@ class code_maker: public base_player
         code_maker() {};
         ~code_maker() {};
 
-        void init_game();
-        void play_round();
+        void init_game(state_t value);
+        void play_round(state_t &value);
 
     private:
-        
+        // Variables
+        code_t _key;
+
+        // Functions
+        void _check_code(state_t &value);
+        std::vector<color> _list_colors(code_t value);
 };
+
+/*! \class code_maker code_maker.h "code_maker.h"
+ *  \brief This is the code-maker class.
+ * This class inheritates from pure virtual class base_player in which
+ * the dependency-injection pattern is defined for two main functions.
+ */
+
+/*! \fn void code_maker::init_game(state_t value) 
+ *  \brief Init game
+ *  This function initialises the object according to
+ *  the information in \param value.
+ *  \param value: A value respresenting a game state - it is of type "state_t"
+ */
+
+/*! \fn void code_maker::play_round(state_t &value)
+ *  \brief Play round
+ *  This function executes a round in the game. It purpose is to provide
+ *  feedback to the player "code-breaker" regarding their code guess. The
+ *  code is part of the game-state value contained in \param value.
+ *  \param value: Reference to game state - it is of type "state_t"
+ */
 
 #endif // CODE_MAKER_H
