b4d2f7a9748ab7cea602982d7430a54cbe434818
Mauricio Vanegas <mauriciovanegash@gmail.com>
2020-09-17 11:45:23 +0200

    Change: Adapt code-breaker class to parent class base_player !wip

--- src/code_breaker.cpp
+++ src/code_breaker.cpp
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include "code_breaker.h"
 
-void code_breaker::init_game()
+void code_breaker::init_game(state_t value)
 {
     printf("Breaker: Initialising game!!!\n");
 }
 
-void code_breaker::play_round()
+void code_breaker::play_round(state_t &value)
 {
-    printf("Breaker: Playing round %d!!!\n", _round);
+    value.code = code_t(blue, blue, yellow, yellow);
     _round++;
 }
--- src/code_breaker.h
+++ src/code_breaker.h
@@ -1,6 +1,7 @@
 #ifndef CODE_BREAKER_H
 #define CODE_BREAKER_H
 
+#include <vector>
 #include "base_player.h"
 
 class code_breaker: public base_player
@@ -9,11 +10,31 @@ class code_breaker: public base_player
         code_breaker() {};
         ~code_breaker() {};
 
-        void init_game();
-        void play_round();
+        void init_game(state_t value);
+        void play_round(state_t &value);
 
     private:
-        
 };
+
+/*! \class code_breaker code_breaker.h "code_breaker.h"
+ *  \brief This is the code-breaker class.
+ * This class inheritates from pure virtual class base_player in which
+ * the dependency-injection pattern is defined for two main functions.
+ */
+
+/*! \fn void code_breaker::init_game(state_t value) 
+ *  \brief Init game
+ *  This function initialises the object according to
+ *  the information in \param value.
+ *  \param value: A value respresenting a game state - it is of type "state_t"
+ */
+
+/*! \fn void code_breaker::play_round(state_t &value)
+ *  \brief Play round
+ *  This function executes a round in the game. It purpose is to provide
+ *  generate a code in the light of the information provided by the code-
+ *  maker in \param value.
+ *  \param value: Reference to game state - it is of type "state_t"
+ */
 
 #endif // CODE_BREAKER_H
